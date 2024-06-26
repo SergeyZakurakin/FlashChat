6156ac05122c230f341468403659fbc2e5b1439f
SergeyZakurakin <117861728+SergeyZakurakin@users.noreply.github.com>
2024-06-26 13:14:13 +0100

    Start new Project

---
+++ FlashChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlashChat
+//
+//  Created by Sergey Zakurakin on 26/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
